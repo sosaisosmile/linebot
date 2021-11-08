b9d1621f4c6edd60d7046b3f3a83299b7391f3aa
sosaisosmile <92450191+sosaisosmile@users.noreply.github.com>
2021-11-08 22:53:52 +0700

    Update pub.php

--- pub.php
+++ pub.php
@@ -1,8 +1,8 @@
  <?php
  function pubMqtt($topic,$msg){
-       $APPID= "FixitCenter/"; //enter your appid
-     $KEY = "0ZcxhUOlSSrdtMt"; //enter your key
-    $SECRET = "JS9MqcQ2QyuujStrl7Y7Nqim2"; //enter your secret
+       $APPID= "JARVISLED/"; //enter your appid
+     $KEY = "kTwFSJxnqE0IXLC"; //enter your key
+    $SECRET = "oQR5YB7Ck5Z1qi4d3mzSDqFEu"; //enter your secret
     $Topic = "$topic"; 
       put("https://api.netpie.io/microgear/".$APPID.$Topic."?retain&auth=".$KEY.":".$SECRET,$msg);
  
