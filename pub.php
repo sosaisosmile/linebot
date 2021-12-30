de8adcc6d92619d9a242e4a6f562ffb492e5a73a
sosaisosmile <92450191+sosaisosmile@users.noreply.github.com>
2021-12-30 17:59:58 +0700

    Update pub.php

--- pub.php
+++ pub.php
@@ -4,7 +4,7 @@
      $KEY = "3y6lKWs3EHtF72s"; //enter your key
     $SECRET = "Qy4fZypRGpw7G8dSvc5dy3UlP"; //enter your secret
     $Topic = "$topic"; 
-      put("https://api.netpie.io".$APPID.$Topic."?retain&auth=".$KEY.":".$SECRET,$msg);
+      put("https://2015.netpie.io/app/JARVISLED".$APPID.$Topic."?retain&auth=".$KEY.":".$SECRET,$msg);
  
   }
  function getMqttfromlineMsg($Topic,$lineMsg){
